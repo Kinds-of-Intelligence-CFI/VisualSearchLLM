ab11bc34e00427910b5d0a8e33af2b483fe47c67
John Burden <jjb205@login-q-1.data.cluster>
2025-03-19 10:35:09 +0000

    Fixed missing directory flag in processAllJobs.py

--- processAllJobs.py
+++ processAllJobs.py
@@ -5,12 +5,13 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+    parser.add_argument("-d", "--directory", required=True)
     parser.add_argument("-g", "--gpt4o", action="store_true")
-    parser.add_argument("-c" "--claude", action="store_true")
+    parser.add_argument("-c", "--claude", action="store_true")
     parser.add_argument("-l", "--llama", action="store_true")
     parser.add_argument("-rc", "--rowsandcolumns", action="store_true")
     parser.add_argument("-q", "--quadrants", action="store_true")
-    parser.add_argument("-c", "--coordinates", action="store_true")
+    parser.add_argument("-co", "--coordinates", action="store_true")
     args=parser.parse_args()
 
 
@@ -24,11 +25,13 @@ if __name__ == '__main__':
  
     if args.gpt4o:
         subprocess.run(["python3", "processBatchResults.py", "-d", args.directory, "-m", "gpt-4o", processFlag])
-    if parser.claude:
+    if args.claude:
         subprocess.run(["python3", "processBatchResults.py", "-d", args.directory, "-m", "claude-sonnet", processFlag])
   
-    if parser.llama:
+    if args.llama:
+        subprocess.run(["python3", "combineLlama.py", "-d", args.directory, "-m", "llama11B"])
         subprocess.run(["python3", "processBatchResults.py", "-d", args.directory, "-m", "llama11B", processFlag])
+        subprocess.run(["python3", "combineLlama.py", "-d", args.directory, "-m", "llama90B"])
         subprocess.run(["python3", "processBatchResults.py", "-d", args.directory, "-m", "llama90B", processFlag])
     print("Finished")
 
